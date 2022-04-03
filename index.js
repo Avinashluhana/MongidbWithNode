4af5ba0590b845610d841e8e671ec64306307b42
avinashluhana <aviluhana@gmail.com>
2022-04-03 16:34:32 +0500

    basic functions of mongodb

---
+++ index.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/mongoWithNode")
+  .then(() => {
+    console.log("Connected Succesfully with the mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const schema = new mongoose.Schema({
+  name: String,
+  rollnbr: Number,
+  enrolled: Boolean,
+});
+
+const collection = new mongoose.model("studentData", schema);
+
+const adder = async () => {
+  const insertData = new collection({
+    name: "Avinash Kumar",
+    rollnbr: 17,
+    enrolled: true,
+  });
+  await insertData.save();
+};
+
+const finder = async () => {
+  const findData = await collection.find();
+  console.log(findData);
+};
+
+adder();
+finder();
